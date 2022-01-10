a1cb0d968fdc06f2bd149ba2b6ef689ad6412639
FearlessIIX <84805129+FearlessIIX@users.noreply.github.com>
2022-01-10 13:24:57 -0500

    Update Player.ts

--- src/routes/game/Player.ts
+++ src/routes/game/Player.ts
@@ -2,13 +2,19 @@
 
 import { writable } from "svelte/store"
 
+// These represent the players starting coordinates
+const starting = {
+    x: 10,
+    y: 10
+}
+
 export const Player = {
     panel: {
-        x: writable(5),
-        y: writable(5)
+        x: writable(starting.x),
+        y: writable(starting.y)
     },
     absolute: {
-        x: writable(5),
-        y: writable(5)
+        x: writable(starting.x),
+        y: writable(starting.y)
     }
 }
